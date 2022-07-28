96e0e72aa51ebd4838957f5517d84a2f92d2be9b
ltmyth <latimmark@gmail.com>
2022-07-28 14:39:03 +0300

    Add Personel urls to CIMS Project urls

--- cims_core/cims/cims/urls.py
+++ cims_core/cims/cims/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('patients/', include("patients.urls")),
+    path('personnel/', include("personnel.urls")),
 ]
