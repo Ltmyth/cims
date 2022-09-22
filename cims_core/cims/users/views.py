1c1a5d674a4938f59206928b777fdcf8bf5681e2
ltmyth <latimmark@gmail.com>
2022-09-22 13:00:48 +0300

    Add flash messages

--- cims_core/cims/users/views.py
+++ cims_core/cims/users/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-#user database
+# flash messages
+from django.contrib import messages
+# user database
 from django.contrib.auth.models import User
-#authenticate
+# authenticate
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
 # Create your views here.
@@ -29,27 +31,35 @@ def login_user(request):
         if user is not None:
             # login
             auth_login(request,user)
+            messages.success(request, 'Login Success')
             #redirect
             return redirect('/home')
         else:
-            # No backend authenticated the credentials
+            # No authenticated the credentials
+            messages.error(request, 'Wrong Username or Password.')
             return render(request, 'users/user_login.html')
     else:
+        messages.success(request, 'Account created.')
         return render(request, 'users/user_login.html')
 
 @login_required
 def logout_user(request):
     auth_logout(request)
-    # Redirect to a success page.
-    return render(request, 'users/user_login.html')
+    messages.success(request, 'You have logged out. Bye!')
+    return redirect('/login')
 
 def create_user(request):
     user_name = request.POST['username']
     user_password = request.POST['password']
     user_email = request.POST['email']
-    user = User.objects.create_user(user_name, user_email, user_password)
-    # Redirect to a success page.
-    return render(request, 'users/login.html')
+    if User.objects.filter(username = user_name).first():
+        messages.info(request, 'Account already exists')
+        return render(request, 'users/user_create.html')
+    else:
+        messages.success(request, 'Account created.')
+        user = User.objects.create_user(user_name, user_email, user_password)
+        # Redirect to a success page.
+        return render(request, 'users/user_login.html')
 
 def create_user_page(request):
     # Redirect to a success page.
