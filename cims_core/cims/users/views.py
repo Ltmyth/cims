1f9d3bfce350461881eab7f986ab166e98d22007
ltmyth <latimmark@gmail.com>
2022-09-30 20:49:09 +0300

    Corrected Patient update bug

--- cims_core/cims/users/views.py
+++ cims_core/cims/users/views.py
@@ -174,9 +174,10 @@ def delete_patient(request):
 
 def update_patient(request):
     user_name = request.POST['Patient_Name']
+    p_id = request.POST['Patient_ID']
     ailment = request.POST['ailment']
     diagnosis = request.POST['diagnosis']
-    user = Patient.objects.get(full_name =user_name)
+    user = Patient.objects.get(id=p_id)
     user.full_name = user_name
     user.diagnosis=diagnosis
     user.symptoms=ailment
